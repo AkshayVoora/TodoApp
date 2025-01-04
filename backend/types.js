3238200a1750676ee7f0242c1419b587db3a0490
AkshayVoora <145536033+AkshayVoora@users.noreply.github.com>
2025-01-04 09:21:20 -0500

    Added Zod

---
+++ backend/types.js
@@ -0,0 +1,16 @@
+const zod = require( "zod");
+
+
+const createTodo = zod.object({
+    tittle:zod.string(),
+    description:zod.string()
+})
+
+const updateTodo = zod.object({
+    id:zod.number()
+})
+
+module.exports={
+    createTodo: createTodo,
+    updateTodo: updateTodo
+}
